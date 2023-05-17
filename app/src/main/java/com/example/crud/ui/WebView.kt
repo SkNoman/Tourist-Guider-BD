03ddbee8ac7c896a66d63077b9d2290f3269d46d
Sk Noman <sknoman77@gmail.com>
2023-05-17 17:23:40 +0600

    update get location settings

--- app/src/main/java/com/example/crud/ui/WebView.kt
+++ app/src/main/java/com/example/crud/ui/WebView.kt
@@ -1,39 +1,35 @@
 package com.example.crud.ui
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationManager
-import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.View
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.crud.BuildConfig
 import com.example.crud.R
 import com.example.crud.base.BaseFragmentWithBinding
 import com.example.crud.databinding.FragmentWebViewBinding
-import com.example.crud.utils.FusedLocation
 import com.example.crud.utils.showCustomToast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 class WebView : BaseFragmentWithBinding<FragmentWebViewBinding>
     (FragmentWebViewBinding::inflate){
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     var searchLink = "https://www.google.com/maps/search/narby+hotels/"
     private var lat:Double = 0.0
     private var long:Double = 0.0
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         binding.ivBackBtn.setOnClickListener{
             findNavController().popBackStack()
         }
@@ -43,70 +39,69 @@ class WebView : BaseFragmentWithBinding<FragmentWebViewBinding>
             searchLink = "https://www.google.com/maps/search/nearby+tourist+spot/"
         }
 
+        getCurrentLocation()
 
-        if (!checkPermission()){
-            Log.e("is_enter","yes")
-            Toast.makeText(requireContext(),getString(R.string.location_access_required), Toast.LENGTH_SHORT).show()
-            findNavController().popBackStack()
-            val intent = Intent()
-            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-            val uri = Uri.fromParts(
-                "package",
-                BuildConfig.APPLICATION_ID,
-                null
-            )
-            intent.data = uri
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-        }
+    }
+
+    private fun getCurrentLocation() {
+
         if (checkPermission()){
-            if (!isLocationEnabled()){
+            if (isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                    val location:Location?=it
+                    if (location==null){
+                        Toast(requireContext()).showCustomToast(getString(
+                            R.string.something_went_wrong),requireActivity())
+                    }else{
+                        showMaps(location.latitude,location.longitude)
+                    }
+                }
+            }
+            else{
+                //setting open here
                 findNavController().popBackStack()
                 Toast.makeText(requireContext(),getString(R.string.turn_on_location), Toast.LENGTH_SHORT).show()
                 val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
                 startActivity(intent)
+            }
+        }
+        else{
+            //request permission here
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION){
+            if (grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation()
             }else{
-                //everything fine then get lat long
-                getLatLong()
+                //show
+                requestPermission()
             }
         }
+    }
 
-
-//        lat = 23.8103
-//        long = 90.4125
-        //showMaps(lat,long)
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 7777
     }
 
 
-    private  fun getLatLong(){
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            val fusedLocationProviderClient: FusedLocationProviderClient =
-                LocationServices.getFusedLocationProviderClient(requireContext())
-            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-                try {
-                    Log.d("latLongFused", "Lat: ${it.latitude}, Long: ${it.longitude}")
-                    lat = it.latitude
-                    long = it.longitude
-                    if (lat != 0.0){
-                        showMaps(lat,long)
-                    }else{
-                        getLatLong()
-                    }
-                } catch (e: Exception) {
-                    Log.d("latLongException", e.toString())
-                }
-            }
-        }
-
-    }
     private fun isLocationEnabled():Boolean{
         val locationManager: LocationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(
