1862ae3035724ec578b5d2b7f077b69281607e5f
Abdullah Al Noman <39592494+SkNoman@users.noreply.github.com>
2023-07-09 14:22:30 +0600

    Add google sign in client

--- app/src/main/java/com/example/crud/ui/login_sign_up/Login.kt
+++ app/src/main/java/com/example/crud/ui/login_sign_up/Login.kt
@@ -1,10 +1,12 @@
 package com.example.crud.ui.login_sign_up
 
 
+import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.findNavController
 import com.example.crud.R
@@ -14,7 +16,13 @@ import com.example.crud.utils.CheckNetwork
 import com.example.crud.utils.SharedPref
 import com.example.crud.utils.Validation
 import com.example.crud.utils.showCustomToast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class Login : BaseFragmentWithBinding<FragmentLoginBinding>
@@ -22,6 +30,7 @@ class Login : BaseFragmentWithBinding<FragmentLoginBinding>
 {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -43,6 +52,24 @@ class Login : BaseFragmentWithBinding<FragmentLoginBinding>
                 Toast(requireContext()).showCustomToast(getString(
                     R.string.please_turn_on_internet_connection),requireActivity())
           }
+        }
+
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(),gso)
+
+
+        binding.btnLogin.setOnClickListener{
+            if (CheckNetwork(requireContext()).isNetworkConnected){
+                signInGoogle()
+            }else{
+                Toast(requireContext()).showCustomToast(getString(
+                    R.string.please_turn_on_internet_connection),requireActivity())
+            }
         }
 
         binding.tvRegister.setOnClickListener{
@@ -70,6 +97,47 @@ class Login : BaseFragmentWithBinding<FragmentLoginBinding>
         }
         return "OK"
     }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handelResults(task)
+        }
+    }
+
+    private fun handelResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account: GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }
+        }else{
+            Toast.makeText(requireContext(),task.exception.toString(),Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(credentials).addOnCompleteListener{
+            if (it.isSuccessful){
+                account.displayName?.let { it1 ->
+                    SharedPref.sharedPrefManger(requireContext(),
+                        it1,"name")
+                }
+                findNavController().navigate(R.id.fragmentDashboard)
+            }else{
+                Toast.makeText(requireContext(),it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
     private fun login(email:String,password:String) {
         auth.signInWithEmailAndPassword(email, password)
